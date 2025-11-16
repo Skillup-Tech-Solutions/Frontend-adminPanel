edd429b2f14cf6595594bc50217e88367cc88f1d
syed.ahamed <syed.ahamed@MGLXINMAC9K3R7.local>
2025-11-16 20:43:14 +0530

    added base url railway fix3

--- src/Config/Config.tsx
+++ src/Config/Config.tsx
@@ -2,7 +2,7 @@ const config = {
   // BASE_URL: import.meta.env.VITE_APP_BASE_URL,
   // BASE_URL: "http://localhost:5000/api/",
   // BASE_URL_MAIN: "http://localhost:5000",
-  BASE_URL: "https://skill-backend.railway.internal/api/",
-  BASE_URL_MAIN: "https://skill-backend.railway.internal.in",
+  BASE_URL: "https://backend.skilluptechbuzz.in/api/",
+  BASE_URL_MAIN: "https://backend.skilluptechbuzz.in",
 };
 export default config;
